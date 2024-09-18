c75603fdf290b08e3da3b7b10e465fcdc625f0d5
24shrouk <143081315+24shrouk@users.noreply.github.com>
2024-09-18 19:46:12 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ st.title("🎈 My new app")
 name=st.text_input("Enter your name")
 btn= st.button("show")
 if btn:
-  st.Write(f'Hello {name}')
+  st.write(f'Hello {name}')
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
