2a3fed23f02131058dd1f6961d336ff8c32fa2b8
24shrouk <143081315+24shrouk@users.noreply.github.com>
2024-09-18 22:12:41 +0300

    Update calculate_area.py

--- pages/calculate_area.py
+++ pages/calculate_area.py
@@ -1,1 +1,15 @@
-
+import streamlit as st
+st.header("Calculate Area")
+choose=st.selectbox('choose the shape ',['rectangle','circle'])
+if choose == 'rectangle':
+  h=st.number_input('Enter the length',min_value=1,max_value=100)
+  w=st.number_input('Enter the width',min_value=1,max_value=100)
+  area=w*h
+elif choose == 'circle':
+  r=st.number_input('Enter the radius',min_value=1,max_value=100)
+  area=3.14*r*r
+btn=st.button('calculate')
+if btn:
+  with st.spinner('Loading....'):
+    #st.time.sleep(2)
+    st.write(f'The area is {area}')
